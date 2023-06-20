87b8e4589621aa341b56b84cfa9bef7ac9d17c6e
Florin Olteanu <olteanuflorin86@gmail.com>
2023-06-20 18:44:17 +0300

    adding initial Beer Order Manager

---
+++ src/main/java/com/olteanuflorin86/msscbeerorderservicev1/services/BeerOrderManager.java
@@ -0,0 +1,9 @@
+package com.olteanuflorin86.msscbeerorderservicev1.services;
+
+import com.olteanuflorin86.msscbeerorderservicev1.domain.BeerOrder;
+
+public interface BeerOrderManager {
+	
+	BeerOrder newBeerOrder(BeerOrder beerOrder);
+
+}

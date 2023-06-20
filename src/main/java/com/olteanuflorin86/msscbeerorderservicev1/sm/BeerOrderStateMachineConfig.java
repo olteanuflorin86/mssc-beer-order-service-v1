22f738c1ce323303ebafa6f95cb6e98e60f5b9eb
Florin Olteanu <olteanuflorin86@gmail.com>
2023-06-20 20:08:51 +0300

    correct property name to fix build

--- src/main/java/com/olteanuflorin86/msscbeerorderservicev1/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/com/olteanuflorin86/msscbeerorderservicev1/sm/BeerOrderStateMachineConfig.java
@@ -21,7 +21,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
 
 	private final Action<BeerOrderStatusEnum, BeerOrderEventEnum>  validateOrderAction;
 	
-	private final Action<BeerOrderStatusEnum, BeerOrderEventEnum>  allocateOrderRequest;
+	private final Action<BeerOrderStatusEnum, BeerOrderEventEnum>  allocateOrderAction;
 	
 	@Override
 	public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states)
@@ -52,7 +52,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
         	.and().withExternal()
         		.source(BeerOrderStatusEnum.VALIDATED).target(BeerOrderStatusEnum.ALLOCATION_PENDING)
         		.event(BeerOrderEventEnum.ALLOCATE_ORDER)
-        		.action(allocateOrderRequest);
+        		.action(allocateOrderAction);
     }
 
 }
